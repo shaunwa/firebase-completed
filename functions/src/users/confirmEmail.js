8c9d6dba7fb990ab96c2fc0585df74b2f98cf207
Shaun W <shaun.p.wassell@gmail.com>
2020-05-16 13:22:27 -0400

    04.09 End

---
+++ functions/src/users/confirmEmail.js
@@ -0,0 +1,40 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+export const confirmEmail = functions.https.onRequest(async (req, res) => {
+    const confirmationHash = req.query.conf;
+    const auth = admin.auth();
+    const store = admin.firestore();
+
+    const querySnapshot = await store.collection('temporaryUsers')
+        .where('confirmationHash', '==', confirmationHash)
+        .get();
+
+    if (querySnapshot.size === 0) {
+        return res.redirect('http://localhost:3000/email-confirmation/failure');
+    }
+
+    const temporaryUserDoc = querySnapshot.docs[0];
+
+    const {
+        authUid,
+        emailAddress,
+        firstName,
+        lastName,
+        bio,
+    } = temporaryUserDoc.data();
+
+    await auth.updateUser(authUid, { emailVerified: true });
+    await store.collection('users')
+        .doc(authId)
+        .set({
+            emailAddress,
+            firstName,
+            lastName,
+            bio,
+        });
+    await store.collection('temporaryUsers')
+        .doc(temporaryUserDoc.id)
+        .delete();
+    return res.redirect('http://localhost:3000/email-confirmation/success');
+});
--- functions/src/users/functions.js
+++ functions/src/users/functions.js
@@ -1,2 +1,3 @@
 export { createAccount } from './createAccount';
 export { sendVerificationEmail } from './sendVerificationEmail';
+export { confirmEmail } from './confirmEmail';
