1af3bd8f2c1ac5929ea6a9efa47895c9aad8c2e8
Shaun W <shaun.p.wassell@gmail.com>
2020-05-15 23:05:54 -0400

    04.03 End

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
+require('regenerator-runtime/runtime');
 const functions = require('firebase-functions');
+const builtFunctions = require('./build');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+Object.keys(builtFunctions).forEach(functionName => {
+    exports[functionName] = builtFunctions[functionName];
+})
